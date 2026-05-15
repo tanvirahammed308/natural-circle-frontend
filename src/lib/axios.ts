15f015fe0a45fd092bdfe90aeb8388b3eaab5b69
tanvirahammed308 <imtanvir.email@gmail.com>
2026-05-15 10:56:36 +0600

    fixed interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -17,7 +17,7 @@ api.interceptors.request.use(
   async (config) => {
     const user = auth.currentUser;
     if (user) {
-      const token = await user.getIdToken(true);
+      const token = await user.getIdToken();
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
